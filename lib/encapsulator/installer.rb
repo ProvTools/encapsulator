b929d9589da8d73a561b188df2a234893c8af8d8
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-09-15 17:43:59 +0100

    fix minor installer issue

--- lib/encapsulator/installer.rb
+++ lib/encapsulator/installer.rb
@@ -1,17 +1,17 @@
 module Encapsulator
-  module Installer
-    def install_fedora
+  class Installer
+    def self.install_fedora
       system 'wget', 'http://download.virtualbox.org/virtualbox/5.1.22/VirtualBox-5.1-5.1.22_115126_fedora25-1.x86_64.rpm', out: $stdout, err: $stdout
       system 'sudo', 'dnf', '-y', '-v', 'install', './VirtualBox-5.1-5.1.22_115126_fedora25-1.x86_64.rpm', out: $stdout, err: $stdout
       system 'sudo', 'dnf', '-y', '-v', 'install', 'vagrant', out: $stdout, err: $stdout
     end
 
-    def install_ubuntu
+    def self.install_ubuntu
     	system 'sudo', 'apt', '-y', 'install', 'virtualbox', 'virtualbox-ext-pack', out: $stdout, err: $stdout
     	system 'sudo', 'apt', '-y', 'install', 'vagrant', out: $stdout, err: $stdout
     end
 
-    def install_mac
+    def self.install_mac
     	system 'brew', 'cask', 'install', 'virtualbox', out: $stdout, err: $stdout
     	system 'brew', 'cask', 'install', 'vagrant', out: $stdout, err: $stdout
     	system 'brew', 'cask', 'install', 'vagrant-manager', out: $stdout, err: $stdout
