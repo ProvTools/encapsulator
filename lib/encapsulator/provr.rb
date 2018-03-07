13cafe417f9f1adba528a83bc8a0431890f313e7
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2018-03-07 20:11:26 +0000

    save json to disk + auto select cran mirror

--- lib/encapsulator/provr.rb
+++ lib/encapsulator/provr.rb
@@ -1,19 +1,24 @@
 module Encapsulator
   module ProvR
     def ProvR.run script
-    	require 'rinruby'
+      require 'rinruby'
       r = RinRuby.new(echo: false)
+      r.eval "chooseCRANmirror(ind=81)"
       r.eval "install.packages('devtools')"
-    	r.eval "require('devtools')"
-    	r.eval "install_github('provtools/provR', ref='dev')"
-    	r.eval "require('provR')"
-      r.eval "prov.capture('#{script}', save=TRUE)"
+      r.eval "require('devtools')"
+      r.eval "install_github('provtools/provR', ref='dev')"
+      r.eval "require('provR')"
+      r.eval "prov.capture('#{script}')"
+      r.eval "f <- file('prov.json')"
+      r.eval "writeLines(prov.json(), f)"
+      r.eval "close(f)"
     end
 
     def ProvR.tidy script
       require 'rinruby'
       r = RinRuby.new(echo: false)
-    	r.eval "install.packages('formatR', repos = 'http://cran.rstudio.com')"
+      r.eval "chooseCRANmirror(ind=81)"
+      r.eval "install.packages('formatR', repos = 'http://cran.rstudio.com')"
       r.eval "require('formatR')"
       r.eval "tidy_file('#{script}')"
     end
